d140c40cad69322d5b06487439bfb924fcc9b9ba
Ledum <ledumibaakee@gmail.com>
2019-10-19 23:13:14 +0100

    added gitignore and extra changes

--- trading/trading/settings.py
+++ trading/trading/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'trading.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
     }
 }
 
